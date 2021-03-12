30434f77b3b999c172d8893ff93eb649c58bd1cf
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-12 16:53:42 -0700

    update r code

--- CrisisCleanupSVI.R
+++ CrisisCleanupSVI.R
@@ -1,13 +1,16 @@
 # install packages
 install.packages("dplyr")
 install.packages("ggplot2")
-install.packages("leaflet")
 
-# packages
+# load packages
 library(dplyr)
 library(ggplot2)
-library(leaflet)
 
+# 2018 SVI data
+svi <- read.csv(file = "/users/danny/documents/capstone/SVI2018_US.csv")
+View(svi)
+
+# Crisis Cleanup data
 ccd <- read.csv(file = "/users/danny/documents/capstone/ccd.csv")
 View(ccd)
 
