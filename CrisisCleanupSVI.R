b4f22f69c6f3e6210b7d9e5b5d8454328515028b
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-12 16:36:59 -0700

    updating r and sql

--- CrisisCleanupSVI.R
+++ CrisisCleanupSVI.R
@@ -1,58 +1,13 @@
 # install packages
+install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("leaflet")
 
 # packages
+library(dplyr)
 library(ggplot2)
 library(leaflet)
 
-# notes
-# histogram with colored tails
+ccd <- read.csv(file = "/users/danny/documents/capstone/ccd.csv")
+View(ccd)
 
-# svi data
-svi <- read.csv(file = "/Users/danny/Documents/Capstone/SVI2018_US.csv")
-View(svi)
-
-# incident count by incident type
-icit <- read.csv(file = "/Users/danny/Documents/Capstone/IncidentCountByIncidentType.csv")
-View(icit)
-
-hist(icit$incident_count, xlab = "Incident Type", ylab = "Incident Count")
-
-icit$incident_type <- factor(icit$incident_type, levels = c("hurricane", "flood", "tornado", "tropical_storm", "virus",
-                                                            "wind", "flood_tstorm", "fire", "snow", "volcano",
-                                                            "rebuild", "mudslide"))
-icit$incident_count <- factor(icit$incident_count, levels = icit$incident_count[order(icit$incident_count, decreasing = FALSE)])
-
-ggplot(data = icit, aes(x = incident_type, y = incident_count)) + 
-  geom_bar(stat = "identity")
-
-# incident count by incident name
-icin <- read.csv(file = "/Users/danny/Documents/Capstone/IncidentCountByIncidentName.csv")
-View(icin)
-
-# flood
-flood <- subset(icin, incident_type == "flood")
-View(flood)
-ggplot(data = flood, aes(x = incident_name, y = incident_count)) +
-  geom_bar(stat = "identity")
-
-# hurricane
-hurricane <- subset(icin, incident_type == "hurricane" & incident_name != "Hurricane Dorian (Bahamas)")
-View(hurricane)
-my_hist <- hist(hurricane$incident_count, breaks = 100, plot = F)
-my_color <- ifelse(my_hist$breaks < -10, rgb(0.2,0.8,0.5,0.5), ifelse(my_hist$breaks >= 10, "purple", rgb(0.2,0.2,0.2,0.2)))
-plot(my_hist, col = my_color, border = F, main = "", xlab = "value of the variable", xlim = c(0, 100))
-
-# tornado
-tornado <- subset(icin, incident_type == "tornado")
-View(tornado)
-
-# tropical_storm
-tropical_storm <- subset(icin, incident_type == "tropical_storm")
-
-# virus
-virus <- subset(icin, incident_type == "virus")
-
-# wind
-wind <- subset(icin, incident_type == "wind")
--- CrisisCleanupSVI.sql
+++ CrisisCleanupSVI.sql
@@ -1,65 +1,17 @@
--- INCIDENT COUNT BY INCIDENT TYPE
-select distinct
-II.incident_type,
-count (II.incident_type) as INCIDENT_COUNT,
-min (WW.svi) as MINSVI,
-avg (WW.svi)::numeric(10,5) as AVGSVI,
-max (WW.svi) as MAXSVI
-from incident_incidents II
-inner join worksite_worksites WW on II.id = WW.incident_id
-where II.start_at >= '2018-01-01'
-group by II.incident_type
-order by II.incident_type
-;
-
--- INCIDENT COUNT BY ORGANIZATION
-select distinct
-OO.name as ORGANIZATION_NAME,
-count (II.incident_type) as INCIDENT_COUNT,
-min (WW.svi) as MINSVI,
-avg (WW.svi)::numeric(10,5) as AVGSVI,
-max (WW.svi) as MAXSVI
-from incident_incidents II
-inner join worksite_worksites WW on II.id = WW.incident_id
-inner join organization_organizations_incidents OOI on II.id = OOI.incident_id
-inner join organization_organizations OO on OOI.organization_id = OO.id
-where II.start_at >= '2018-01-01'
-group by ORGANIZATION_NAME
-order by INCIDENT_COUNT desc
-;
-
--- INCIDENT COUNT BY INCIDENT NAME
-select distinct
-II.incident_type,
-II.name as INCIDENT_NAME,
-to_char (II.start_at, 'YYYY-MM-DD') as INCIDENT_START,
-count (II.incident_type) as INCIDENT_COUNT,
-min (WW.svi) as MINSVI,
-avg (WW.svi)::numeric(10,5) as AVGSVI,
-max (WW.svi) as MAXSVI
-from incident_incidents II
-inner join worksite_worksites WW on II.id = WW.incident_id
-where II.start_at >= '2018-01-01'
-group by II.incident_type, II.name, II.start_at
-order by INCIDENT_COUNT desc
-;
-
-select distinct * from worksite_worksites where svi_id = '25832';
-select distinct * from location_svi;
-select distinct * from incident_incidents II where II.start_at >= '2018-01-01';
-select * from worksite_worksites WW where WW.svi_id = '25832';
-
--- INCIDENT BY SVI
-select
-LS.place_name,
-to_char (II.start_at, 'YYYY-MM-DD') as INCIDENT_START,
-count (WW.id) as WORKSITE_COUNT,
-count (distinct II.case_label) as INCIDENT_COUNT,
-WW.svi
-from incident_incidents II
-inner join worksite_worksites WW on II.id = WW.incident_id
-inner join location_svi LS on WW.svi_id = LS.id
-where II.start_at >= '2018-01-01'
-group by LS.place_name, II.start_at, WW.svi
-order by WORKSITE_COUNT desc, INCIDENT_COUNT desc
-;
+SELECT ww.id, wwwtsp.work_type_key,  ww.svi, ww.location, ww.created_at, ww.incident_id, ii.name, ii.incident_type
+FROM worksite_worksites AS ww
+LEFT JOIN worksite_worksites_work_types_statuses_phases AS wwwtsp
+ON ww.id = wwwtsp.worksite_id
+LEFT JOIN incident_incidents AS ii
+ON ww.incident_id = ii.id
+WHERE wwwtsp.invalidated_at IS NULL
+AND ww.incident_id IN(
+  WITH incident_query AS
+  (SELECT ww.incident_id, COUNT(ww.incident_id) AS worksite_count
+  FROM worksite_worksites AS ww
+  GROUP BY ww.incident_id)
+  SELECT incident_id
+  FROM incident_query
+  WHERE worksite_count >=1000
+)
+ORDER BY ww.id, ww.created_at;
