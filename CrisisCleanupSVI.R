a0bf8ca1d3817f2695aaa6f10dc0bfbc92888c2d
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-16 15:36:09 -0600

    add pivot table

--- CrisisCleanupSVI.R
+++ CrisisCleanupSVI.R
@@ -1,17 +1,27 @@
 # install packages
+install.packages("tidyverse")
 install.packages("dplyr")
 install.packages("ggplot2")
 install.packages("pivottabler")
+install.packages("tidyverse")
 
 # load packages
+library(tidyverse)
 library(dplyr)
 library(ggplot2)
 library(pivottabler)
 
+us_states <- map_data("state")
+view(us_counties)
+
+c <- ggplot(data = us_states, mapping = aes(x = long, y = lat, group = group, fill = region))
+c + geom_polygon() + guides(fill = FALSE)
+
 # 2018 SVI data
 svi <- read.csv(file = "/users/danny/documents/capstone/SVI2018_US.csv")
 View(svi)
-
+svilocation <- data.frame(svi$STATE, svi$COUNTY)
+View(svilocation)
 
 # Crisis Cleanup data
 ccd <- read.csv(file = "/users/danny/documents/capstone/ccd.csv")
@@ -24,7 +34,6 @@ it <- unique(it)
 it <- data.frame(it$ccd.incident_type)
 it <- data.frame(table(unlist(it)))
 it <- subset(it, it$Freq > 1)
-View(it)
 ggplot(it, aes(x = reorder(it$Var1, -it$Freq), y = it$Freq)) +
   geom_bar(stat = "identity")
 
@@ -33,11 +42,8 @@ wt <- data.frame(ccd$work_type_key)
 wt <- data.frame(table(unlist(wt)))
 wt <- subset(wt, wt$Freq >= 20000)
 wt <- wt[order(-wt$Freq), ]
-View(wt)
 ggplot(wt, aes(x = reorder(Var1, -Freq), y = Freq)) + 
   geom_bar(stat = "identity")
-
-test1 <- subset(ccd, incident_type == "wind" & work_type_key == "mold_remediation")
 
 # pivot table
 itwt <- data.frame(ccd$incident_type, ccd$work_type_key)
@@ -45,7 +51,6 @@ itwt <- subset(itwt, (ccd.incident_type == "flood" | ccd.incident_type == "hurri
                         ccd.incident_type == "wind") & 
                  (ccd.work_type_key == "tarp" | ccd.work_type_key == "muck_out" | ccd.work_type_key == "trees" | 
                     ccd.work_type_key == "debris" | ccd.work_type_key == "mold_remediation"))
-View(itwt)
 pt <- PivotTable$new()
 pt$addData(itwt)
 pt$addColumnDataGroups("ccd.incident_type")
@@ -67,16 +72,6 @@ windpt <- subset(ccd, incident_type == "wind" & (work_type_key == "debris" | wor
                                                      work_type_key == "muck_out" | work_type_key == "tarp" | 
                                                      work_type_key == "trees"))
 
-# hurricane
-hurricane <- subset(ccd, incident_type == "hurricane")
-hurricane <- data.frame(hurricane$work_type_key)
-hurricane <- data.frame(table(unlist(hurricane)))
-hurricane <- hurricane[order(-hurricane$Freq), ]
-hurricane <- subset(hurricane, Freq >= 10000)
-View(hurricane)
-ggplot(hurricane, aes(x = reorder(Var1, -Freq), y = Freq)) + 
-  geom_bar(stat = "identity")
-
 # flood
 flood <- subset(ccd, incident_type == "flood")
 flood <- data.frame(flood$work_type_key)
@@ -87,15 +82,15 @@ View(flood)
 ggplot(flood, aes(x = Var1)) + 
   geom_bar()
 
-# tropical storm
-ts <- subset(ccd, incident_type == "tropical_storm")
-ts <- data.frame(ts$work_type_key)
-ts <- data.frame(table(unlist(ts)))
-ts <- ts[order(-ts$Freq), ]
-ts <- subset(ts, Freq >= 70)
-View(ts)
-ggplot(ts, aes(x = Var1)) + 
-  geom_bar()
+# hurricane
+hurricane <- subset(ccd, incident_type == "hurricane")
+hurricane <- data.frame(hurricane$work_type_key)
+hurricane <- data.frame(table(unlist(hurricane)))
+hurricane <- hurricane[order(-hurricane$Freq), ]
+hurricane <- subset(hurricane, Freq >= 10000)
+View(hurricane)
+ggplot(hurricane, aes(x = reorder(Var1, -Freq), y = Freq)) + 
+  geom_bar(stat = "identity")
 
 # tornado
 tornado <- subset(ccd, incident_type == "tornado")
