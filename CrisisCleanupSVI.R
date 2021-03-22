5ca37410331af9c64d09f24113a8678a99ecec40
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-22 16:46:15 -0600

    update SVI charts

--- CrisisCleanupSVI.R
+++ CrisisCleanupSVI.R
@@ -74,24 +74,48 @@ ggplot(svicounty, aes(x = svicounty.COUNTY, y = svicounty.RPL_THEMES, fill = svi
   xlab("") +
   labs(y = "svi")
 
+# SVI flood
+sviflood <- subset(svi, STATE == "MICHIGAN" & RPL_THEMES != "-999" & 
+                     (COUNTY == "Midland" | COUNTY == "Gladwin" | COUNTY == "Clare" | 
+                      COUNTY == "Saginaw" | COUNTY == "Arenac" | COUNTY == "Gratiot" | 
+                      COUNTY == "Bay" | COUNTY == "Lenawee" | COUNTY == "Iosco" | 
+                      COUNTY == "Isabella" | COUNTY == "Wayne" | COUNTY == "Muskegon" | 
+                      COUNTY == "Gladwin" | COUNTY == "Charlevoix" | COUNTY == "Berrien" | 
+                      COUNTY == "Washtenaw" | COUNTY == "Macomb" | COUNTY == "Clinton" | 
+                      COUNTY == "Kent" | COUNTY == "St. Clair" | COUNTY == "Livingston"))
+sviflood <- data.frame(sviflood$COUNTY, sviflood$ST_ABBR, sviflood$RPL_THEMES)
+sviflood$ctyst <- paste(sviflood$sviflood.COUNTY, sviflood$sviflood.ST_ABBR, sep = " County, ")
+ggplot(sviflood, aes(x = ctyst, y = sviflood.RPL_THEMES, fill = ctyst)) + 
+  geom_boxplot() + 
+  theme(legend.position = "none", axis.text = element_text(angle = 90)) + 
+  xlab("") + 
+  labs(y = "svi")
+
 # flood
-flood <- subset(ccd, incident_type == "flood" & name == "Michigan Floods, May 2020")
-
-ctabbr <- function() {
-  if flood$state == "Michigan"
-}
-
-flood["ctabbr"] <- ()
-
+flood <- subset(ccd, incident_type == "flood" & name == "Michigan Floods, May 2020" & state == "Michigan")
 ggplot(flood, aes(x = created_at, y = svi)) + 
   geom_point() + 
   theme(plot.title = element_text(hjust = 0.5)) + 
   ggtitle("Michigan Floods 2020") + 
   labs(x = "time", y = "svi")
 
-colnames(flood)
-unique(flood$state)
-unique(flood$county)
+# SVI hurricane
+svihurricane <- subset(svi, RPL_THEMES != "-999" & (STATE == "LOUISIANA" | STATE == "ALABAMA" | STATE == "MISSISSIPPI" | 
+                                                      STATE == "NORTH CAROLINA" | STATE == "GEORGIA" | STATE == "FLORIDA") & 
+                         (COUNTY == "St. Tammy" | COUNTY == "Jefferson" | COUNTY == "Orleans" | COUNTY == "Mobile" | 
+                            COUNTY == "Barldwin" | COUNTY == "Forrest" | COUNTY == "Lamar" | COUNTY == "St. Bernard" | 
+                            COUNTY == "Pearl River" | COUNTY == "Clarke" | COUNTY == "Harrison" | COUNTY == "St. John the Baptist" | 
+                            COUNTY == "Lafourche" | COUNTY == "Hancock" | COUNTY == "Plaquemines" | COUNTY == "Mecklenburg" | 
+                            COUNTY == "Fulton" | COUNTY == "Ascension" | COUNTY == "Wilcox" | COUNTY == "Jackson" | 
+                            COUNTY == "Autauga" | COUNTY == "Stone" | COUNTY == "East Baton Rouge" | COUNTY == "Washington" | 
+                            COUNTY == "George" | COUNTY == "Douglas" | COUNTY == "Dallas" | COUNTY == "Lumpkin" | 
+                            COUNTY == "Escambia" | COUNTY == "Union" | COUNTY == "Dekalb" | COUNTY == "Bartow" | 
+                            COUNTY == "St. Charles" | COUNTY == "Gwinnett" | COUNTY == "Marengo" | COUNTY == "Marshall" | 
+                            COUNTY == "Cobb" | COUNTY == "Paulding" | COUNTY == "Tallapoosa" | COUNTY == "Talladega" | 
+                            COUNTY == "White" | COUNTY == "Terrebonne" | COUNTY == "Shelby" | "Greene" | COUNTY == "Bibb" | 
+                            COUNTY == "Chilton" | COUNTY == "Perry" | COUNTY == "Montgomery"))
+
+unique(svi$COUNTY)
 
 # hurricane
 hurricane <- subset(ccd, incident_type == "hurricane" & name == "Hurricane Zeta")
@@ -101,6 +125,10 @@ ggplot(hurricane, aes(x = created_at, y = svi)) +
   ggtitle("Hurricane Zeta 2020") + 
   labs(x = "time", y = "svi")
   
+colnames(hurricane)
+unique(hurricane$state)
+unique(hurricane$county)
+
 # tornado
 tornado <- subset(ccd, incident_type == "tornado" & name == "Easter/April 2020 Tornadoes")
 ggplot(tornado, aes(x = created_at, y = svi)) + 
